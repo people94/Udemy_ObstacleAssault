828a3311a11ab3168643c73eaa82f6275102b1dc
people94 <bsyu0804@naver.com>
2024-08-07 13:31:12 +0900

    Section 3, 52

--- Source/ObstacleAssault/MovingPlatform.cpp
+++ Source/ObstacleAssault/MovingPlatform.cpp
@@ -15,15 +15,33 @@ AMovingPlatform::AMovingPlatform()
 void AMovingPlatform::BeginPlay()
 {
 	Super::BeginPlay();
-
-	MyVector.X = MyX;
-	MyVector.Y = MyY;
-	MyVector.Z = MyZ;
 }
 
 // Called every frame
 void AMovingPlatform::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+
+	FVector LocalVector = MyVector;
+
+	LocalVector.Z = LocalVector.Z + 100;
+
+	MyVector.Y = MyVector.Y +1;
+
+	SetActorLocation(LocalVector);
+
+	// Move platform forwards
+
+	// Get curent location
+
+	// Add vector to that location
+
+	// Set the location
+
+	// Send platform back if gone too far
+
+	// Check how far we've moved
+
+	// Reverse direction of motion if gone too far
 }
 
